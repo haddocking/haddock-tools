586ed9c390399fd9a2024c6f1d704006a430f246
Rodrigo V Honorato <r.vargashonorato@uu.nl>
2022-07-13 16:36:18 +0200

    Update renumber_model.py

--- renumber_model.py
+++ renumber_model.py
@@ -13,6 +13,7 @@ formatter = logging.Formatter(
 ch.setFormatter(formatter)
 log.addHandler(ch)
 
+
 def pdb2fastadic(pdb_f):
     """
     Write the sequence as a fasta.
@@ -50,8 +51,8 @@ def pdb2fastadic(pdb_f):
             ("DG", "G"),
             ("DC", "C"),
             ("DT", "T"),
-            ]
-        )
+        ]
+    )
     seq_dic = {}
 
     with open(pdb_f) as fh:
@@ -96,9 +97,7 @@ def align(reference, model, output_path):
     for ref_chain, model_chain in zip(seqdic_ref, seqdic_model):
 
         if ref_chain != model_chain:
-            raise Exception(
-                f"Chain mismatch: {ref_chain} != {model_chain}"
-                )
+            raise Exception(f"Chain mismatch: {ref_chain} != {model_chain}")
 
         align_dic[ref_chain] = {}
 
@@ -121,24 +120,22 @@ def align(reference, model, output_path):
 
         identity = (
             str(top_aln).count("|") / float(min(len(seq_ref), len(seq_model)))
-            ) * 100
+        ) * 100
 
         if not any(e for e in top_aln.aligned):
             # No alignment!
             log.warning(
                 f"No alignment for chain {ref_chain} is it protein/dna? "
                 "Matching sequentially"
-                )
-            if all(
-                    "X" in s for s in seq_ref) and all(
-                    "X" in s for s in seq_model):
+            )
+            if all("X" in s for s in seq_ref) and all("X" in s for s in seq_model):
                 # this sequence contains only ligands, do it manually
                 if len(seq_ref) != len(seq_model):
                     # we cannot handle this
                     raise f"Cannot align chain {model_chain}"
                 for ref_res, model_res in zip(
-                        seqdic_ref[ref_chain],
-                        seqdic_model[model_chain]):
+                    seqdic_ref[ref_chain], seqdic_model[model_chain]
+                ):
 
                     align_dic[ref_chain].update({model_res: ref_res})
         else:
@@ -146,23 +143,28 @@ def align(reference, model, output_path):
                 # Identity is very low
                 log.warning(
                     f"Sequence identity of chain {ref_chain} is "
-                    f"{identity:.2f}%, please check the results carefully")
+                    f"{identity:.2f}%, please check the results carefully"
+                )
             else:
                 log.debug(
                     f"Sequence identity between chain {ref_chain} "
                     f"of {reference} and chain {model_chain} of {model} is "
-                    f"{identity:.2f}%")
+                    f"{identity:.2f}%"
+                )
             for ref_segment, model_segment in zip(
-                    aligned_ref_segment, aligned_model_segment):
+                aligned_ref_segment, aligned_model_segment
+            ):
 
                 start_ref_segment, end_ref_segment = ref_segment
                 start_model_segment, end_model_segment = model_segment
 
                 reslist_ref = list(seqdic_ref[ref_chain].keys())[
-                    start_ref_segment:end_ref_segment]
+                    start_ref_segment:end_ref_segment
+                ]
 
                 reslist_model = list(seqdic_model[model_chain].keys())[
-                    start_model_segment:end_model_segment]
+                    start_model_segment:end_model_segment
+                ]
 
                 for _ref_res, _model_res in zip(reslist_ref, reslist_model):
                     align_dic[ref_chain].update({_model_res: _ref_res})
@@ -175,10 +177,10 @@ def renumber_model(model_f, reference_dic):
     Renumber the model according to the reference dictionary.
     """
     new_model_f = Path(model_f).name.replace(".pdb", "_renumbered.pdb")
-    log.info(f'Renumbered model name: {new_model_f}')
+    log.info(f"Renumbered model name: {new_model_f}")
     ignored_dic = {}
-    with open(new_model_f, 'w') as out_fh:
-        with open(model_f, 'r') as fh:
+    with open(new_model_f, "w") as out_fh:
+        with open(model_f, "r") as fh:
             for line in fh.readlines():
                 if line.startswith("ATOM"):
                     chain = line[21]
@@ -196,23 +198,23 @@ def renumber_model(model_f, reference_dic):
                 else:
                     out_fh.write(line)
     for chain in ignored_dic:
-        log.warning(
-            f"Ignored residues {ignored_dic[chain]} in model's chain {chain}")
+        log.warning(f"Ignored residues {ignored_dic[chain]} in model's chain {chain}")
     return new_model_f
+
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("reference", help='')
-    parser.add_argument("model", help='')
+    parser.add_argument("reference", help="")
+    parser.add_argument("model", help="")
     args = parser.parse_args()
     log.setLevel("DEBUG")
 
-    log.info('Getting sequence numbering relationship via BLOSUM62 alignment')
+    log.info("Getting sequence numbering relationship via BLOSUM62 alignment")
     numbering_dic = align(args.reference, args.model, ".")
-    log.info('Renumbering model according to numbering relationship')
+    log.info("Renumbering model according to numbering relationship")
     renumbered_model_f = renumber_model(args.model, numbering_dic)
 
-    log.info('Renumbering complete')
-    log.info('DO NOT trust this renumbering blindly!')
-    log.info('Check the .aln files for more information')
+    log.info("Renumbering complete")
+    log.info("DO NOT trust this renumbering blindly!")
+    log.info("Check the .aln files for more information")
